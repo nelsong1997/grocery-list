f6d6c8d767249ca9c466c1cf022e1d94fa6cc39e
Gabe Nelson <nelsong1997@gmail.com>
2023-03-21 21:30:36 -0400

    some boxes with some words

--- src/List.js
+++ src/List.js
@@ -6,20 +6,43 @@ class List extends React.Component {
 		super()
 		this.state = {
 			property: "value"
-		}	
+		}
+
+		this.displayListCategory = this.displayListCategory.bind(this);
+	}
+
+	displayListCategory(categoryIndex) {
+		let categoryObj = someData.itemCategories[categoryIndex]
+		let categoryName = categoryObj.categoryName
+		let categoryItems = categoryObj.items
+
+		let categoryItemsJSX = []
+
+		for (let item of categoryItems) {
+			categoryItemsJSX.push(
+				<label>{item}</label>
+			)
+		}
+
+		return (
+			<div className="inner-box" style={{outline: "true"}}>
+				<strong><label>{categoryName}</label></strong>
+				{categoryItemsJSX}
+			</div>
+		)
 	}
 
 	render() {
 		return (
 			<div className="center-box">
-				<div className="columns">
+				<div className="columns-box">
 					<div className="column">
-						<div style={{backgroundColor: "blue"}}>uh</div>
-						<div style={{backgroundColor: "red"}}>test</div>
+						{this.displayListCategory(0)}
+						{this.displayListCategory(1)}
 					</div>
 					<div className="column">
-						<div style={{backgroundColor: "green"}}>1</div>
-						<div style={{backgroundColor: "purple"}}>2</div>
+						{this.displayListCategory(2)}
+						{this.displayListCategory(3)}
 					</div>
 				</div>
 			</div>
@@ -28,3 +51,32 @@ class List extends React.Component {
 }
 
 export default List;
+
+let someData = {
+	itemCategories: [
+		{
+			categoryName: "cat1",
+			items: [
+				"apple", "banana", "cheese"
+			]
+		},
+		{
+			categoryName: "cat2",
+			items: [
+				"apple2", "banana2"
+			]
+		},
+		{
+			categoryName: "cat3",
+			items: [
+				"apple3", "banana3"
+			]
+		},
+		{
+			categoryName: "cat4",
+			items: [
+				"apple4", "banana4"
+			]
+		}
+	]
+}
